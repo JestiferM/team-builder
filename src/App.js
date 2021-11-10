95103b39c6deac7c11d854870789b9b8bd118884
JestiferM <jestifermartinez01@gmail.com>
2021-11-09 21:24:34 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -37,3 +37,4 @@ const onChange = (name, value) => {
 }
 
 export default App;
+// testing
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+// testing
 const Form = (props) => {
     
     const handleChange = event => {
